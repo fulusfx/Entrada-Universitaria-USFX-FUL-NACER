3d026a91903e4cc0b1e356f8e8777eec26597af1
fulusfx <naceralaful23@gmail.com>
2025-06-08 10:18:55 -0400

    Update script.js

--- script.js
+++ script.js
@@ -668,7 +668,7 @@ let scrollInterval;
 function startAutoScroll() {
     autoScrollActive = true;
     const scrollDuration = 2000; // ✅ DURACIÓN: 2 segundos de scroll automático
-    const scrollDistance = 10; // ✅ DISTANCIA: Píxeles a desplazarse hacia abajo
+    const scrollDistance = 50000; // ✅ DISTANCIA: Píxeles a desplazarse hacia abajo
     const startTime = Date.now();
     const startPosition = window.pageYOffset;
     
